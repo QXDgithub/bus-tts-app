0ba29870a6d666b8dd5a619c2d587aea512a8377
QXDgithub <167010708+QXDgithub@users.noreply.github.com>
2024-07-12 10:01:04 +0530

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script>
     import { onMount } from 'svelte';
-    import { supabase } from '$lib/supabase';
+    import { supabase } from '$lib/supabaseClient';
     import { goto } from '$app/navigation';
 
     onMount(async () => {
